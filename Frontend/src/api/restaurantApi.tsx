d10ce62e4ccbdc866c35fc5a595c49a9b280f381
Shvam Singh <singhshivam583@yahoo.in>
2024-07-23 16:37:15 +0530

    changes in api

--- Frontend/src/api/restaurantApi.tsx
+++ Frontend/src/api/restaurantApi.tsx
@@ -1,6 +1,6 @@
-import { useQuery } from "react-query";
+// import { useQuery } from "react-query";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // export const useSearchRestaurants = (city?:string) => {
 //     const createSearchRequest = async() => {
