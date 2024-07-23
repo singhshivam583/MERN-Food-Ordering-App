8b8bbf4b1ceab66a8b8629a089b2b0b824b32c1f
Shvam Singh <singhshivam583@yahoo.in>
2024-07-23 16:35:20 +0530

    changes in api

--- Frontend/src/api/myRestaurantApi.tsx
+++ Frontend/src/api/myRestaurantApi.tsx
@@ -1,112 +1,113 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
-import { toast } from 'sonner';
-import { Restaurant } from "../types"
+import { toast } from "sonner";
+import { Restaurant } from "../types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useGetMyRestaurant = () => {
+  const { getAccessTokenSilently } = useAuth0();
 
-    const { getAccessTokenSilently } = useAuth0();
+  const getMyRestaurantRequest = async (): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
 
-    const getMyRestaurantRequest = async(): Promise<Restaurant> => {
-        const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`,{
-            method:"GET",
-            headers: {
-                'Authorization': `Bearer ${accessToken}`
-            }
-        });
+    if (!response.ok) throw new Error("Could not create restaurant");
 
-        if(!response.ok) throw new Error("Could not create restaurant");
+    return response.json();
+  };
 
-        return response.json()
-    };
+  const {
+    data: restaurant,
+    isLoading,
+    error,
+  } = useQuery("fetchMyRestaurant", getMyRestaurantRequest);
 
-    const {
-        data: restaurant, 
-        isLoading, 
-        error
-    } = useQuery('fetchMyRestaurant', getMyRestaurantRequest);
+  if (error) {
+    toast.error("Error while fetching data");
+  }
 
-    if(error){
-        toast.error("Error while fetching data");
-    }
-
-    return { restaurant, isLoading };
-}
+  return { restaurant, isLoading };
+};
 
 export const useCreateMyRestaurant = () => {
-    const { getAccessTokenSilently } = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-    const createMyRestaurantRequest = async(restaurantFormData: FormData): Promise<Restaurant> => {
-        const accessToken = await getAccessTokenSilently();
+  const createMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
 
-        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
-            method: "POST",
-            headers:{
-                Authorization: `Bearer ${accessToken}`,
-            },
-            body: restaurantFormData,
-        })
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
 
-        if(!response.ok) throw new Error("Could not create restaurant");
+    if (!response.ok) throw new Error("Could not create restaurant");
 
-        return response.json()
-    };
+    return response.json();
+  };
 
-    const {
-        mutate: createRestaurant, 
-        isLoading, 
-        isSuccess, 
-        error
-    } = useMutation(createMyRestaurantRequest);
+  const {
+    mutate: createRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(createMyRestaurantRequest);
 
-    if(isSuccess){
-        toast.success("Restaurant created")
-    }
-    if(error){
-        toast.error("Unable to create restaurant");
-    }
+  if (isSuccess) {
+    toast.success("Restaurant created");
+  }
+  if (error) {
+    toast.error("Unable to create restaurant");
+  }
 
-    return {createRestaurant, isLoading};
-}
+  return { createRestaurant, isLoading };
+};
 
 export const useUpdateMyRestaurant = () => {
-    const {getAccessTokenSilently} = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
-    const  updateMyRestaurantRequest = async(restaurantFormData: FormData): Promise<Restaurant> => {
-        const accessToken = await getAccessTokenSilently();
+  const updateMyRestaurantRequest = async (
+    restaurantFormData: FormData
+  ): Promise<Restaurant> => {
+    const accessToken = await getAccessTokenSilently();
 
-        const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
-            method: "PUT",
-            headers:{
-                Authorization: `Bearer ${accessToken}`,
-            },
-            body: restaurantFormData,
-        });
+    const response = await fetch(`${API_BASE_URL}/api/my/restaurant`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: restaurantFormData,
+    });
 
-        if(!response.ok) throw new Error("Could not create restaurant");
+    if (!response.ok) throw new Error("Could not create restaurant");
 
-        return response.json()
-    };
+    return response.json();
+  };
 
-    const {
-        mutate: updateRestaurant, 
-        isLoading, 
-        isSuccess, 
-        error
-    } = useMutation(updateMyRestaurantRequest);
+  const {
+    mutate: updateRestaurant,
+    isLoading,
+    isSuccess,
+    error,
+  } = useMutation(updateMyRestaurantRequest);
 
-    if(isSuccess){
-        toast.success("Restaurant Updated Successfully")
-    }
-    if(error){
-        toast.error("Unable to update restaurant");
-    }
+  if (isSuccess) {
+    toast.success("Restaurant Updated Successfully");
+  }
+  if (error) {
+    toast.error("Unable to update restaurant");
+  }
 
-    return {updateRestaurant, isLoading};
-
-}
-
+  return { updateRestaurant, isLoading };
+};
